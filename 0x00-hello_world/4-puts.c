abf2c90888638dd348dae93699b71d5ec0a4dcaa
abdallah <comofcom@gmail.com>
2023-03-17 03:53:29 +0200

    fix puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
-* The main function 
-* It start the program, print a sentence and return 0
-*/
-#include <string.h>
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
