70d51b86b77acb17ee69752375bec992cf0d3896
abdallah <comofcom@gmail.com>
2023-03-20 08:05:12 +0200

    create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = 'a';
+do
+{
+	putchar(c++);
+} while(c <= 'z');
+putchar('\n');
+return (0);
+}
