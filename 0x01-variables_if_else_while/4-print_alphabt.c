53146a107d74b9f66d0c317ff56caef56d5abefb
abdallah <comofcom@gmail.com>
2023-03-20 08:52:40 +0200

    create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = 'a';
+do if(c != 'q' && c =! 'e') putchar(c++);
+while (c <= 'z');
+putchar('\n');
+return (0);
+}
