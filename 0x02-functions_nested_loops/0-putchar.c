69a992e90b1ade0e78e880bb78686bedbbe00279
aboudeif <comofcom@gmail.com>
2023-03-21 22:42:57 -0700

    create 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -7,6 +7,13 @@
  */
 int main(void)
 {
-printf("_putchar\n");
+char c[8] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
 return (0);
 }
