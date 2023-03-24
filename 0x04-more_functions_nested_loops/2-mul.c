ed9d595d5503efe42df983065ad0db32c469e5c2
aboudeif <comofcom@gmail.com>
2023-03-24 05:16:30 -0700

    create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Entry function
+ * multiplies two integers
+ * @a: int type number
+ * @b: int type number
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
